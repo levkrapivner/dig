bf77d3f1366ee58671da24b31913bc0fb2859b27
levkrapivner <skyrim12345@yahoo.com>
2016-08-23 14:57:24 +0300

    Create ns.py

---
+++ ns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import dns.resolver
+
+myResolver = dns.resolver.Resolver()
+myResolver.nameservers = ['8.8.8.8', '8.8.4.4']
+try:
+        myAnswers = myResolver.query("linuxadmin.biz", "NS")
+        for rdata in myAnswers:
+                print rdata
+except Exception as e:
+        print e
